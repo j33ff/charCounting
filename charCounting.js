8354cdf84812138409482d7d5b8e3901be172465
Jeff FitzGerald <j33ff.fitzgerald@gmail.com>
2017-09-06 19:05:52 +0000

    program is working with multiple characters. pushing completed excersise.

---
+++ charCounting.js
@@ -0,0 +1,16 @@
+function countLetters(inputString){
+  var outputObject = {};
+
+  for (i = 0; i < inputString.length; i++) {
+
+    var char = inputString[i];
+    if(outputObject.hasOwnProperty(char)){
+      outputObject[char] += 1;
+    } else {
+      outputObject[char] = 1;
+    }
+  }
+  return outputObject;
+}
+
+console.log(countLetters(process.argv[2]));
